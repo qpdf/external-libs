b1859c6e9f4be1b2628ef957c89fed8618729e68
Jay Berkenbilt <ejb@ql.org>
2022-03-08 13:12:59 -0500

    Build on windows-2022 (windows-latest)

--- scripts/build-windows.bat
+++ scripts/build-windows.bat
@@ -2,9 +2,9 @@
 @rem Usage: build-windows {32|64}
 setlocal ENABLEDELAYEDEXPANSION
 if %1 == 64 (
-   call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+   call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 ) else (
-   call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+   call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
 )
 set MSYS2_PATH_TYPE=inherit
 C:\msys64\usr\bin\env.exe MSYSTEM=MINGW64 /bin/bash -l %CD%/scripts/build %1
